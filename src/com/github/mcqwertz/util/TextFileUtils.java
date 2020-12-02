3a542806eec1d47474a82321812bb6cc2ca7c3de
MCqwertz <mcqwertz.dev@web.de>
2020-12-02 16:02:55 +0100

    + day 2

--- src/com/github/mcqwertz/util/TextFileUtils.java
+++ src/com/github/mcqwertz/util/TextFileUtils.java
@@ -6,6 +6,7 @@ package com.github.mcqwertz.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
@@ -13,12 +14,27 @@ import java.util.stream.IntStream;
 public class TextFileUtils {
 
 	public static Supplier<IntStream> getNumbers(int day) throws FileNotFoundException {
-		File file = new File("src/com/github/mcqwertz/year2020/input/Day" + day + ".txt");
+		File file = new File("src/com/github/mcqwertz/year2020/input/Day" + day);
 		Scanner scanner = new Scanner(file);
 		ArrayList<String> arrayList = new ArrayList<>();
 		while(scanner.hasNextLine()) {
 			arrayList.add(scanner.nextLine());
 		}
 		return () -> arrayList.stream().mapToInt(Integer::parseInt);
+	}
+
+	public static String[][] getInputDay2() throws FileNotFoundException {
+		File file = new File("src/com/github/mcqwertz/year2020/input/Day2");
+		Scanner scanner = new Scanner(file);
+		ArrayList<String> arrayList = new ArrayList<>();
+		while(scanner.hasNextLine()) {
+			arrayList.add(scanner.nextLine());
+		}
+		String[][] input = new String[arrayList.size()][4];
+		for (int i = 0; i < arrayList.size(); i++) {
+			String [] strings = arrayList.get(i).split("-")[1].replace(":", " ").split(" ");
+			input[i] = new String[]{arrayList.get(i).split("-")[0], strings[0], strings[1], strings[3]};
+		}
+		return input;
 	}
 }
---
+++ src/com/github/mcqwertz/year2020/days/Day2.java
@@ -0,0 +1,47 @@
+//created by MCqwertz
+
+
+package com.github.mcqwertz.year2020.days;
+
+import com.github.mcqwertz.util.TextFileUtils;
+
+import java.io.FileNotFoundException;
+import java.util.function.IntPredicate;
+
+public class Day2 {
+	public static void main(String[] args) throws FileNotFoundException {
+		String[][] input = TextFileUtils.getInputDay2();
+		Day2 day = new Day2();
+		System.out.println("Part 1: " + day.getFirstPart(input));
+		System.out.println("Part 2: " + day.getSecondPart(input));
+
+	}
+
+	private int getFirstPart(String[][] input) {
+		int i = 0;
+		for (String[] strings : input) {
+			IntPredicate isSearched = arg -> arg == strings[2].charAt(0);
+			int int0 = Integer.parseInt(strings[0]);
+			int int1 = Integer.parseInt(strings[1]);
+			int appearances = strings[3].chars().filter(isSearched).toArray().length;
+			if(appearances <= int1 && appearances >= int0) {
+				i++;
+			}
+		}
+		return i;
+	}
+
+	private int getSecondPart(String[][] input) {
+		int i = 0;
+		for (String[] strings : input) {
+			int int0 = Integer.parseInt(strings[0]) - 1;
+			int int1 = Integer.parseInt(strings[1]) - 1;
+			char c = strings[2].charAt(0);
+			if ((strings[3].charAt(int0) == c && strings[3].charAt(int1) != c) ||
+					(strings[3].charAt(int0) != c && strings[3].charAt(int1) == c)) {
+				i++;
+			}
+		}
+		return i;
+	}
+}
