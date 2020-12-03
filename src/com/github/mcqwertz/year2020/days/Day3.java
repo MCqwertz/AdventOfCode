2661a1cabf05020adbdaff2b60e76724f8890fbd
MCqwertz <mcqwertz.dev@web.de>
2020-12-03 17:59:18 +0100

    + used longs

--- src/com/github/mcqwertz/year2020/days/Day3.java
+++ src/com/github/mcqwertz/year2020/days/Day3.java
@@ -16,13 +16,13 @@ public class Day3 {
 
 
 	private static long getSecondResult(char[][] grid) {
-		return Math.multiplyFull(getTrees(grid, 1, 1), getTrees(grid, 3, 1) * getTrees(grid, 5, 1) *
-				getTrees(grid, 7, 1) * getTrees(grid, 1, 2));
+		return getTrees(grid, 1, 1) * getTrees(grid, 3, 1) * getTrees(grid, 5, 1) *
+				getTrees(grid, 7, 1) * getTrees(grid, 1, 2);
 
 	}
 
-	private static int getTrees(char[][] grid, int right, int down) {
-		int trees = 0;
+	private static long getTrees(char[][] grid, int right, int down) {
+		long trees = 0;
 		int coulomb = 0;
 		for (int i = 0; i < grid.length; i = i + down) {
 			coulomb = coulomb + right;
